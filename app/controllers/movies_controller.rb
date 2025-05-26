f80d3c59e603f797a201984fe4c0b38e37c2f03e
rpf13 <rpf13@me.com>
2025-05-26 07:48:20 +0200

    Add edit movie functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,18 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params =
+      params.require(:movie).
+        permit(:title, :ratin, :released_on, :total_gross)
+
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
 end
