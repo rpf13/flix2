cde79c7ffa6bb90c61ddb62bdbcdc5c15a54e31b
rpf13 <rpf13@me.com>
2025-07-07 08:10:13 +0200

    Add stars rating

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -10,4 +10,13 @@ module MoviesHelper
   def year_of(movie)
     movie.released_on.year
   end
+
+  # No longer used
+  # def average_stars(movie)
+  #   if movie.average_stars.zero?
+  #     content_tag(:strong, "No reviews")
+  #   else
+  #     "*" * movie.average_stars.round
+  #   end
+  # end
 end
