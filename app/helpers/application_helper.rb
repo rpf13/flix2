0b2654a005d5ee9b5fea0cd6b08efb7f9e0600e4
rpf13 <rpf13@me.com>
2025-08-19 06:56:27 +0200

    Add user memoization and accout_settings link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def current_user
     # if there is no session, it would return nil and error, therefore the conditional
-    User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
